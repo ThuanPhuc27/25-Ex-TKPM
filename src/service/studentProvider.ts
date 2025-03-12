7d340972197ce7d7dcbf4fb54ffd848a742a0350
Thuc Phan <phanhongthuc7749@gmail.com>
2025-03-12 12:34:10 +0700

    provide jsdoc to the student storing service

--- src/service/studentProvider.ts
+++ src/service/studentProvider.ts
@@ -1,13 +1,25 @@
 import { Students_data } from "../data/index";
 
+/**
+ * Type represents the required fields for the storing data
+ */
 type Student = {
-  studentId: string;
+  studentId: string | number;
   fullName: string;
 };
 
+/**
+ * The primary key of the `Student` table
+ */
 const STUDENT_STORE_KEY = "studentId";
+/**
+ * The attribute containing the name of the student in the `Student` table, used to search.
+ */
 const STUDENT_NAME_FIELD = "fullName";
 
+/**
+ * The possible returning values of {@link checkStudentKey} function
+ */
 const StudentKeyCheckResponse = {
   VALID_KEY: `The key propery "${STUDENT_STORE_KEY}" contains valid value to be added to the database`,
   KEY_NOT_FOUND: `The key property "${STUDENT_STORE_KEY}" not found in the data object`,
@@ -16,51 +28,126 @@ const StudentKeyCheckResponse = {
 
 let dbInstance: IDBDatabase | null = null;
 
-const initializeStudentData = async () => {
+/**
+ * Establishing a connection to the database.
+ * If the database instance is not already connected, it will attempt to connect.
+ *
+ * You could invoke this function before any operation to the IndexedDb,
+ * to reduce possible delay from connecting to the storage.
+ *
+ * @async
+ * @function initializeConnection
+ * @returns {Promise<void>} A promise that resolves when the database connection is established.
+ */
+const initializeConnection = async (): Promise<void> => {
   if (dbInstance === null) {
     dbInstance = await _connect();
   }
 };
 
-const checkStudentKey = async (data: any) => {
+/**
+ * Checks the validity of a student's key.
+ *
+ * This function should be called before calling {@link addStudent},
+ * to ensure that the key of the object is valid to store the object!
+ *
+ * @async
+ * @param data - The data containing the student's key to be checked
+ * @returns {Promise<string>} A promise that resolves to the result of the key check, whose value is in {@link StudentKeyCheckResponse} object.
+ * - {@link StudentKeyCheckResponse.VALID_KEY}: The object has the key that is not existed yet in the database and is ready to be added.
+ * - {@link StudentKeyCheckResponse.EXISTED_KEY}: The object has the key that is already existed in the database.
+ * - {@link StudentKeyCheckResponse.KEY_NOT_FOUND}: The object doesn't have the required key field.
+ */
+const checkStudentKey = async (data: any): Promise<string> => {
   if (dbInstance === null) {
-    await initializeStudentData();
+    await initializeConnection();
   }
   return _checkStudentKey(dbInstance!, data);
 };
 
-/// Throw "ConstraintError" DOMException if the key has existed
-const addStudent = async (student: Student) => {
+/**
+ * Adds a student to the database.
+ *
+ * If the key is not provided, an auto-generated **integer** will be used as key
+ *
+ * If the key is already existed, a {@link DOMException} will be thrown.
+ *
+ * It is recommended to call the function {@link checkStudentKey} and check its result
+ * before calling this function.
+ *
+ * @async
+ * @param {Student} student - The student object to be added to the database.
+ * @returns {Promise<string>} A promise that resolves and return the **student's key** when the student is added.
+ */
+const addStudent = async (student: Student): Promise<string> => {
   if (dbInstance === null) {
-    await initializeStudentData();
+    await initializeConnection();
   }
   return _addStudent(dbInstance!, student);
 };
 
-const upsertStudent = async (student: Student) => {
+/**
+ * Upserts a student in the database.
+ *
+ * This function will insert a new student if the student does not exist,
+ * or update the existing student if the student already exists.
+ *
+ * You can check if the student has already existed using {@link checkStudentKey}.
+ *
+ * @async
+ * @param {Student} student - The student object to be upserted.
+ * @returns {Promise<string>} A promise that resolves and return the **student's key** when the student is upserted.
+ */
+const upsertStudent = async (student: Student): Promise<string> => {
   if (dbInstance === null) {
-    await initializeStudentData();
+    await initializeConnection();
   }
   return _upsertStudent(dbInstance!, student);
 };
 
-const removeStudentById = async (studentId: string) => {
+/**
+ * Removes a student by ID from the database.
+ *
+ * Do nothing/do not throw anything if the student whose id is not in the database.
+ *
+ * @async
+ * @param {string} studentId - The ID of the student to be removed.
+ * @returns {Promise<true>} A promise that resolves to true when the student is removed.
+ */
+const removeStudentById = async (studentId: string): Promise<true> => {
   if (dbInstance === null) {
-    await initializeStudentData();
+    await initializeConnection();
   }
   return _removeStudentById(dbInstance!, studentId);
 };
 
-const fetchAllStudents = async () => {
+/**
+ * Fetches all students from the database.
+ *
+ * @async
+ * @returns {Promise<Student[]>} A promise that resolves to an array of all students in the database.
+ */
+const fetchAllStudents = async (): Promise<Student[]> => {
   if (dbInstance === null) {
-    await initializeStudentData();
+    await initializeConnection();
   }
   return _fetchAllStudents(dbInstance!);
 };
 
-const findStudentsByName = async (studentName: string) => {
+/**
+ * Finds students by their name.
+ *
+ * This function searches for students in the database whose names **contain** the provided `studentName`.
+ *
+ * An empty array will be returned if there is no student containing that "name".
+ *
+ * @async
+ * @param {string} studentName - The name of the student to search for.
+ * @returns {Promise<Student[]>} A promise that resolves to an array of students matching the provided name.
+ */
+const findStudentsByName = async (studentName: string): Promise<Student[]> => {
   if (dbInstance === null) {
-    await initializeStudentData();
+    await initializeConnection();
   }
   return _findStudentsByName(dbInstance!, studentName);
 };
@@ -69,7 +156,7 @@ export {
   Student,
   STUDENT_STORE_KEY,
   StudentKeyCheckResponse,
-  initializeStudentData,
+  initializeConnection,
   checkStudentKey,
   addStudent,
   upsertStudent,
@@ -96,7 +183,7 @@ async function _checkStudentKey(db: IDBDatabase, data: any): Promise<string> {
     request.onsuccess = (ev: Event) =>
       resolve(StudentKeyCheckResponse.EXISTED_KEY);
     request.onerror = (ev: Event) => {
-      resolve(StudentKeyCheckResponse.KEY_NOT_FOUND);
+      resolve(StudentKeyCheckResponse.VALID_KEY);
     };
   });
 }
